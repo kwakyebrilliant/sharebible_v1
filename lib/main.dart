a74f9cb4493f70a6bbbd569ddda35451a1471ba2
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2024-12-29 09:36:11 +0300

    main updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sharebible/theme/dark_mode.dart';
+import 'package:sharebible/theme/light_mode.dart';
 import 'package:sharebible/theme/theme_notifier.dart';
 
 void main() {
@@ -18,9 +20,19 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false);
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      darkTheme: darkMode,
+      theme: lightMode,
+      themeMode:
+          themeNotifier.currentTheme == lightMode
+              ? ThemeMode.light
+              : ThemeMode.dark,
+    );
   }
 }
