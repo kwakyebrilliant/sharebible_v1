5b3dde55fa3764e4cfd384e7fab65cd6b97e8dd9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-01-01 01:11:38 +0300

    landing page updates

--- lib/component/landing_page.dart
+++ lib/component/landing_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sharebible/screens/Bible/bible_screen.dart';
 import 'package:sharebible/screens/Home/home_screen.dart';
+import 'package:sharebible/screens/Notification/notification_screen.dart';
 import 'package:sharebible/screens/Settings/setting_screen.dart';
 
 class LandingPage extends StatefulWidget {
@@ -16,6 +17,7 @@ class _LandingPageState extends State<LandingPage> {
   final List<Widget> _pages = [
     const HomeScreen(),
     const BibleScreen(),
+    const NotificationScreen(),
     const SettingScreen(),
   ];
 
@@ -36,8 +38,8 @@ class _LandingPageState extends State<LandingPage> {
           },
           selectedItemColor: Colors.blueGrey,
           unselectedItemColor: Theme.of(context).colorScheme.inversePrimary,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
+          // showSelectedLabels: false,
+          // showUnselectedLabels: false,
           type: BottomNavigationBarType.fixed,
           items: [
             BottomNavigationBarItem(
@@ -97,6 +99,29 @@ class _LandingPageState extends State<LandingPage> {
                   borderRadius: BorderRadius.circular(10.0),
                   boxShadow:
                       _currentIndex == 2
+                          ? [
+                            BoxShadow(
+                              color: Colors.white.withValues(alpha: 0.2),
+                              blurRadius: 8,
+                            ),
+                          ]
+                          : null,
+                ),
+                child: const Icon(Icons.notifications_rounded),
+              ),
+              label: 'Notifications',
+            ),
+            BottomNavigationBarItem(
+              icon: Container(
+                padding: const EdgeInsets.all(8.0),
+                decoration: BoxDecoration(
+                  color:
+                      _currentIndex == 3
+                          ? Theme.of(context).colorScheme.primaryContainer
+                          : null,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow:
+                      _currentIndex == 3
                           ? [
                             BoxShadow(
                               color: Colors.white.withValues(alpha: 0.2),
