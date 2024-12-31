ca76ebaf47cf6ccebab9c39ed311219419b7a1de
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-01-01 01:17:09 +0300

    bible screen updates

--- lib/screens/Bible/bible_screen.dart
+++ lib/screens/Bible/bible_screen.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 
-class BibleScreen extends StatefulWidget {
+class BibleScreen extends StatelessWidget {
   const BibleScreen({super.key});
 
   @override
-  State<BibleScreen> createState() => _BibleScreenState();
-}
-
-class _BibleScreenState extends State<BibleScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // List to store the Bible books in order
+    List<String> bibleBooks = [];
+
+    List<String> filteredBooks = [];
+
+    //search controller
+    TextEditingController searchController = TextEditingController();
+
+    // Default Bible version
+    String selectedVersion = 'KJV';
+    List<String> availableVersions = ['KJV', 'ASV', 'WEB', 'NET'];
+
+    return const Placeholder();
   }
 }
