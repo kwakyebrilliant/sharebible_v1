69b281fe2b0d8497edcdb2fc21bdc92fd77259ba
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-01-05 19:17:18 +0300

    notification list updates

--- lib/screens/Notification/notification_screen.dart
+++ lib/screens/Notification/notification_screen.dart
@@ -170,6 +170,88 @@ class _NotificationScreenState extends State<NotificationScreen> {
                     ],
                   ),
                 ),
+
+                //Container around notiification list 2
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8.0, top: 8.0),
+                  decoration: BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                        width: 1.0,
+                      ),
+                    ),
+                    color: Theme.of(context).colorScheme.surface,
+                  ),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment:
+                        MainAxisAlignment.start, // Keep this as start
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          left: 20.0,
+                          right: 10.0,
+                          bottom: 10.0,
+                        ),
+                        child: Icon(
+                          Icons.menu_book_rounded,
+                          color: Colors.pink,
+                          size: 28.0,
+                        ),
+                      ),
+                      Expanded(
+                        // Use Expanded to fill available space
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Exodus 1:12',
+                              style: GoogleFonts.inter(
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.w600,
+                                color: Colors.blueGrey,
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.all(3.0),
+                              width: MediaQuery.of(context).size.width - 100.0,
+                              child: Text(
+                                'Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book',
+                                style: GoogleFonts.inter(
+                                  fontSize: 12.0,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+
+                            // Add some spacing above
+                            Padding(
+                              padding: const EdgeInsets.only(top: 4.0),
+
+                              // Align to the right
+                              child: Align(
+                                alignment: Alignment.centerRight,
+
+                                // Time text
+                                child: Text(
+                                  '9:12 am',
+                                  style: GoogleFonts.inter(
+                                    fontSize: 8.0,
+                                    fontWeight: FontWeight.w700,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
