e57b12b4d4d3abe2047de1403eac3d1e70d80f28
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2025-01-08 23:02:23 +0300

    items added to container

--- lib/screens/Settings/faq/faq_screen.dart
+++ lib/screens/Settings/faq/faq_screen.dart
@@ -44,13 +44,70 @@ class FaqScreen extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                // Got questions text
+                // Header text here
                 Text(
                   'Got Questions?',
                   style: GoogleFonts.inter(
                     fontSize: 28.0,
                     fontWeight: FontWeight.bold,
                     color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+
+                // Subheader text
+                Text(
+                  'Some answers to frequently asked questions.',
+                  style: GoogleFonts.inter(
+                    fontSize: 14.0,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+
+                // Container wrapped in a padding
+                Padding(
+                  padding: const EdgeInsets.only(top: 20.0),
+
+                  // Container here
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey[100],
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+
+                    // Row for container items
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 15.0,
+                            top: 15.0,
+                            bottom: 15.0,
+                            right: 10.0,
+                          ),
+                          child: Container(
+                            height: 35.0,
+                            width: 35.0,
+                            decoration: BoxDecoration(
+                              color: Colors.blueGrey,
+                              borderRadius: BorderRadius.circular(20.0),
+                            ),
+                            child: const Icon(
+                              Icons.question_answer_sharp,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+
+                        // Text within container here
+                        const Text(
+                          'Browse through these quick answers.',
+                          style: TextStyle(
+                            fontSize: 14.0,
+                            color: Colors.blueGrey,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
